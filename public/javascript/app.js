231af5e489a869d76888a45749e0d3ccf4b9cfc9
idriss <idriss.zouhair01@gmail.com>
2023-07-22 14:16:07 +0200

    update 3

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,1 +1,40 @@
 
+const contactForm = document.querySelector('.contact-form');
+
+let firstname = document.getElementById ('firstname');
+let lastname = document.getElementById ('lastname');
+let tel = document.getElementById ('tel');
+let email = document.getElementById ('email');
+let message = document.getElementById ('message');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData = {
+        firstname: firstname.value,
+        lastname: lastname.value,
+        tel: tel.value,
+        email: email.value,
+        message: message.value
+    }
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+        if (xhr.responseText == 'success') {
+            alert('Email Sent');
+            firstname.value = '';
+            lastname.value = '';
+            tel.value = '';
+            email.value = '';
+            message.value = '';
+        } else {
+            alert ('Something went wrong!');
+        }
+    }
+
+    xhr.send(JSON.stringify(formData))
+});
--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const expressLayouts = require('express-ejs-layouts');
+const nodemailer = require('nodemailer');
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
-// STATIC FILES
+
+// MIDDLEWARE
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/javascript', express.static(__dirname + 'public/javascript'));
@@ -12,24 +15,56 @@ app.use('/img', express.static(__dirname + 'public/img'));
 app.use('/audio', express.static(__dirname + 'public/audio'));
 app.use('/fonts', express.static(__dirname + 'public/fonts'));
 app.use('/static', express.static('public'));
+app.use(express.json());
+
 
 // VIEW ENGINE 
-// app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
 
 // ROUTES 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index');
 })
 app.get('/index', (req, res) => {
-    res.render('index')
+    res.render('index');
 })
 app.get('*', (req, res)=> {   
     res.send("404, Page not found");
 })
     
 
+// POST
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'idriss.zouhair.dev@gmail.com',
+            pass: process.env.NODEMAILER_PASSWORD,
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'idriss.zouhair.dev@gmail.com',
+        message : `Message from ${req.body.email}: ${req.body.message}`,
+        text: req.body.message
+    } 
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success');
+        }
+    })
+})
+
+
 // PORT 
-app.listen(3000, function() {
-    console.log("Server is up at port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}`);
 })

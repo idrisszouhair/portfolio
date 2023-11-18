edf10f1989983a242f0ce7448cd603b8feeb1f5f
idriss <idriss.zouhair01@gmail.com>
2023-11-18 10:24:50 +0100

    nodemailer port update for heroku

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.post('/', (req, res) => {
 
     const transporter = nodemailer.createTransport({
         service: 'hotmail',
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.NODEMAILER_ACCOUNT,
             pass: process.env.NODEMAILER_PASSWORD,
